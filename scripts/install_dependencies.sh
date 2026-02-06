74b796667a98334b4e8cba1e6a4bb88b7a35c343
Hardik Rangucha <hardikrangucha11@gmail.com>
2026-02-06 21:09:18 +0530

    Hard fix npm EACCES issue in CodeDeploy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-cd /home/ec2-user/node-app
-npm install --no-fund --no-audit
+set -e
+
+APP_DIR="/home/ec2-user/node-app"
+
+echo "Fixing ownership..."
+chown -R ec2-user:ec2-user $APP_DIR
+
+echo "Removing old lock file..."
+rm -f $APP_DIR/package-lock.json
+
+echo "Installing dependencies as ec2-user..."
+sudo -u ec2-user bash << EOF
+cd $APP_DIR
+npm install
+EOF
